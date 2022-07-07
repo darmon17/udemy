f59d9c80817259da5cbac3866a96894df81b8204
darmon17 <heriwinong01@gmail.com>
2022-07-07 17:18:57 +0700

    function parameter

---
+++ function-parameter.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func sayHallo(firstname string, lastname string, umur int) {
+	fmt.Println("Hallo ", firstname, lastname, "umur anda ", umur)
+}
+
+func main() {
+	sayHallo("Heri", "Sudarmono", 2022-1999)
+}
